46b0e16aa011e5dbea6c96b57288aba20ae5929e
AndBarata <146003937+AndBarata@users.noreply.github.com>
2024-01-11 01:35:27 +0000

    pasta data

---
+++ Projeto/data/parse_clock.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+# Open the text file and read its contents
+version = "V2/"
+file_name = version + 'clockA_no_offset'
+with open(file_name + ".txt", 'r') as file:
+    lines = file.readlines()
+
+offsets = []
+rates = []
+delays = []
+
+for i, line in enumerate(lines):
+    if line.find('offset') != -1:
+        offsets.append(float(line.split(':')[1]))
+        
+    elif line.find('rate') != -1:
+        rates.append(float(line.split(':')[1]))
+    
+    elif line.find('delay') != -1:
+        delays.append(float(line.split(':')[-1]))
+
+    print("DEBUG: ", i)
+
+    #print(f'\nLine {i}: {line}\n{offsets}, {rates}, {delays}')
+
+# Convert the matches to a DataFrame
+data = pd.DataFrame({
+    'offset': offsets,
+    'rate': rates,
+    'delay': delays
+})
+
+# Save the DataFrame to a CSV file
+data.to_csv(file_name + '.csv', index=False)
+
+df = pd.read_csv(file_name + '.csv')
+print(df.tail())
